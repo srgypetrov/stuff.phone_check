853a00c1fb31d9ad133e4333e5561600bd67f6c2
Sergey Petrov <desoulate777@gmail.com>
2018-04-21 14:26:57 +0300

    Add Makefile

---
+++ Makefile
@@ -0,0 +1,12 @@
+all: registry bottle rust
+
+registry:
+	python get_registry.py
+
+bottle:
+	curl -O https://raw.githubusercontent.com/bottlepy/bottle/0.12.13/bottle.py
+
+rust:
+	cargo build --manifest-path rust/Cargo.toml --release
+
+.PHONY: all registry bottle rust
--- get_registry.py
+++ get_registry.py
@@ -6,20 +6,19 @@ from urllib.error import URLError, HTTPError
 from urllib.request import urlopen
 
 
-LOCAL_DIR = 'phone_registry'
-BASE_URL = 'https://www.rossvyaz.ru/docs/articles/'
-FILENAMES = ['DEF-9x', 'ABC-8x', 'ABC-3x', 'ABC-4x']
+DIR = 'phone_registry'
+URL = 'https://www.rossvyaz.ru/docs/articles/'
+FILENAMES = ['DEF-9x.csv', 'ABC-8x.csv', 'ABC-3x.csv', 'ABC-4x.csv']
 
 
 def get_registry_file(filename):
     print(f'Downloading {filename} ...')
     try:
-        response = urlopen(f'{BASE_URL}{filename}.csv')
+        response = urlopen(f'{URL}{filename}')
     except (URLError, HTTPError) as err:
         print(f'Error: {err}')
         return
-    filepath = f'{LOCAL_DIR}/{filename}.csv'
-    with closing(response), open(filepath, 'w', encoding='utf-8') as file:
+    with closing(response), open(f'{DIR}/{filename}', 'w', encoding='utf-8') as file:
         while True:
             chunk = response.read(4096).decode('windows-1251')
             if not chunk:
@@ -29,9 +28,9 @@ def get_registry_file(filename):
 
 
 def download_files():
-    if os.path.exists(LOCAL_DIR):
-        shutil.rmtree(LOCAL_DIR)
-    os.mkdir(LOCAL_DIR)
+    if os.path.exists(DIR):
+        shutil.rmtree(DIR)
+    os.mkdir(DIR)
     with Pool() as pool:
         pool.map(get_registry_file, FILENAMES)
 
