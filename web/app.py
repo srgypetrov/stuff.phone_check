46de3d34823bb6c2b952b57a24b4fbf5eff4d492
Sergey Petrov <desoulate777@gmail.com>
2018-05-12 15:33:55 +0300

    Fix web app

--- web/app.py
+++ web/app.py
@@ -1,33 +1,67 @@
+import json
 import os
+import subprocess
 
 import bottle
 
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(DIR)
+LOG_PATH = os.path.join(DIR, 'check.log')
+PID_PATH = os.path.join(BASE_DIR, 'check.pid')
+META_PATH = os.path.join(BASE_DIR, 'registry/meta.json')
+CHECK_PATH = os.path.join(BASE_DIR, 'rust/target/release/check_hash')
+
 app = bottle.default_app()
-base_path = os.path.abspath(os.path.dirname(__file__))
-log_path = os.path.join(base_path, 'check.log')
-bottle.TEMPLATE_PATH.insert(0, os.path.join(base_path, 'views'))
+bottle.TEMPLATE_PATH.insert(0, DIR)
 
 
 @bottle.route('/')
 def index():
-    return bottle.template('index')
+    return bottle.template('app')
+
+
+@bottle.route('/start', method='POST')
+@bottle.route('/start/', method='POST')
+def start():
+    if os.path.isfile(PID_PATH):
+        return {
+            'success': False,
+            'message': 'Process already running'
+        }
+    valid, data = validate_hashes(bottle.request.json)
+    if not valid:
+        return {
+            'success': False,
+            'message': data
+        }
+    with open(LOG_PATH, 'w') as logfile:
+        subprocess.Popen([CHECK_PATH, *data], stdout=logfile)
+    return {
+        'success': True
+    }
 
 
 @bottle.route('/result')
 @bottle.route('/result/')
 def result():
-    if not os.path.exists(log_path):
-        bottle.redirect('/')
-    return bottle.template('result')
-
-
-@bottle.route('/result_progress')
-@bottle.route('/result_progress/')
-def result_progress():
-    with open(log_path) as logfile:
+    if not os.path.isfile(LOG_PATH):
+        return {}
+    with open(LOG_PATH) as logfile:
         log = logfile.read()
-    data = process_log(log)
-    return bottle.template('result_progress', **data)
+    return process_log(log)
+
+
+def get_progress(line_info):
+    fileno, filename, lineno = line_info.split()
+    with open(META_PATH) as metafile:
+        meta = json.load(metafile)
+    lines_count = meta.get(filename, 0)
+    percent = float(lineno) / float(lines_count) * 100
+    return {
+        'percent': round(percent),
+        'fileno': int(fileno),
+    }
 
 
 def parse_log(log):
@@ -45,9 +79,42 @@ def parse_log(log):
 
 
 def process_log(log):
+    data = {}
     log_data = parse_log(log)
-    return log_data
+    line = log_data.get('line')
+    runtime = log_data.get('runtime')
+    phones = log_data.get('phone')
+    unknown = log_data.get('unknown')
+    error = log_data.get('error')
+    if line is not None:
+        data.update(get_progress(line))
+    if runtime is not None:
+        spent = int(runtime)
+        data.update({'spent': f'{spent // 60}:{spent % 60}'})
+    if phones is not None:
+        data.update({'phones': [p.split() for p in phones]})
+    if unknown is not None:
+        data.update({'unknown': unknown})
+    if error is not None:
+        data.update({'error': error})
+    return data
+
+
+def validate_hashes(hashes_data):
+    valid_hashes = []
+    if not hashes_data.strip():
+        return False, 'No hashes specified'
+    for item in hashes_data.split(','):
+        item = item.strip()
+        if len(item) != 40:
+            return False, f'Value is not 40-digit number: {item}'
+        try:
+            int(item, 16)
+        except ValueError:
+            return False, f'Value is not hexidecimal number: {item}'
+        valid_hashes.append(item)
+    return True, valid_hashes
 
 
 if __name__ == "__main__":
-    bottle.run(host='localhost', port=8000, debug=True)
+    bottle.run(host='127.0.0.1', port=8000, debug=True)
